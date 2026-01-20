1256c46337ccc77b1fd9987a360386b5854b85f4
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-01-20 23:19:44 +0800

    nvim: harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,8 +1,7 @@
 require('harpoon').setup()
-require('telescope').load_extension('harpoon')
 
-vim.keymap.set('n', '<leader>h', ':Telescope harpoon marks <CR>', {})
+vim.keymap.set('n', '<leader>h', require('harpoon.ui').toggle_quick_menu, {})
 vim.keymap.set('n', '<leader>m', require('harpoon.mark').add_file, {})
 vim.keymap.set('n', '<leader>n', require('harpoon.ui').nav_next, {})
-vim.keymap.set('n', '<leader>p', require('harpoon.ui').nav_prev, {})
+vim.keymap.set('n', '<leader>N', require('harpoon.ui').nav_prev, {})
 
