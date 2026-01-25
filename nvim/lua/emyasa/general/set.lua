c5a16d9dbf33818b1c497f096c5f0b18f9700755
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-01-25 19:11:26 +0800

    nvim: general

--- nvim/lua/emyasa/general/set.lua
+++ nvim/lua/emyasa/general/set.lua
@@ -6,5 +6,15 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 8
 
-vim.opt.guicursor = "n-v-c:block,i:ver25-blinkon1"
+vim.opt.guicursor = table.concat({
+  "n:block",                              -- Normal: non-blinking block
+  "v:block",                              -- Visual: non-blinking block
+  "c:block",                              -- Command: non-blinking block
+  "i:block-blinkwait700-blinkoff400-blinkon250",   -- Insert: blinking block
+  "ci:block-blinkwait700-blinkoff400-blinkon250",
+  "ve:block-blinkwait700-blinkoff400-blinkon250",
+  "r:hor20",                              -- Replace: non-blinking horizontal
+  "cr:hor20",
+  "o:hor50",                              -- Operator-pending: non-blinking horizontal
+}, ",")
 
