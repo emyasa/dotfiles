608860e7285450e693b328f1b0d482f7bf8671cc
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-01-25 17:11:15 +0800

    nvim: oil

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -1,11 +1,12 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>oh", require("oil.view").toggle_hidden, {})
 
 require("oil").setup({
     keymaps = {
         ["<C-c>"] = false,
     },
     view_options = {
-        show_hidden = true,
+        show_hidden = false,
     },
 })
 
