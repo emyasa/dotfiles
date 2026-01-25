78265e190208b834ef6a82fa2ba01b11eda7fb7a
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-01-25 15:51:31 +0800

    raycast: obsidian hotkey

--- raycast/script-commands/obsidian-hotkey.sh
+++ raycast/script-commands/obsidian-hotkey.sh
@@ -11,82 +11,44 @@
 osascript <<'EOF'
 tell application "System Events"
     set obsidianRunning to (name of processes) contains "Obsidian"
+    if obsidianRunning then
+        tell process "Obsidian"
+            set windowCount to count of windows
+        end tell
+    end if
 end tell
 
--- If Obsidian is running and has a window, bring to front and return early
-if obsidianRunning then
-    tell application "System Events"
-        tell process "Obsidian"
-            if (count of windows) > 0 then
-                set frontmost to true
-                -- Get screen size
-                    tell application "Finder"
-                        set screenBounds to bounds of window of desktop
-                    end tell
-
-                    set screenWidth to item 3 of screenBounds
-                    set screenHeight to item 4 of screenBounds
-
-                    set targetWidth to screenWidth / 3
-                    set targetX to screenWidth - targetWidth
-                    set targetY to 0
-
-                    -- Resize the front window
-                    tell front window
-                        set position to {targetX, targetY}
-                        set size to {targetWidth, screenHeight}
-                    end tell
-                return
-            end if
-        end tell
+if obsidianRunning and windowCount > 0 then
+    tell application "Obsidian"
+        activate
+        return
     end tell
 end if
 
--- Activate Obsidian
-tell application "Obsidian"
-	activate
-end tell
-
-delay 0.3
-
+do shell script "open -a Obsidian"
 tell application "System Events"
-	tell process "Obsidian"
+    tell process "Obsidian"
 
-		-- If there are no windows, open a new one using menu
-		if (count of windows) is 0 then
-			tell menu bar 1
-				tell menu bar item "File"
-					tell menu "File"
-						click menu item "New Window"
-					end tell
-				end tell
-			end tell
-			delay 0.3
-		end if
+        repeat until (count of windows) > 0
+            delay 0.01
+        end repeat
 
-		-- Double-check if we now have a window
-		if (count of windows) is 0 then
-			return
-		end if
+        tell application "Finder"
+            set screenBounds to bounds of window of desktop
+        end tell
 
-		-- Get screen size
-		tell application "Finder"
-			set screenBounds to bounds of window of desktop
-		end tell
+        set screenWidth to item 3 of screenBounds
+        set screenHeight to item 4 of screenBounds
 
-		set screenWidth to item 3 of screenBounds
-		set screenHeight to item 4 of screenBounds
+        set targetWidth to screenWidth / 3
+        set targetX to screenWidth - targetWidth
+        set targetY to 0
 
-		set targetWidth to screenWidth / 3
-		set targetX to screenWidth - targetWidth
-		set targetY to 0
-
-		-- Resize the front window
-		tell front window
-			set position to {targetX, targetY}
-			set size to {targetWidth, screenHeight}
-		end tell
-	end tell
+        tell front window
+            set position to {targetX, targetY}
+            set size to {targetWidth, screenHeight}
+        end tell
+    end tell
 end tell
 EOF
 
