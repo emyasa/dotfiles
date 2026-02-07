c10345a42527c6af0e04a89c1fdd308ae704f227
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-02-07 20:50:18 +0800

    nvim

--- nvim/lua/emyasa/go/lsp.lua
+++ nvim/lua/emyasa/go/lsp.lua
@@ -2,6 +2,7 @@ local mason_bin = vim.fn.stdpath("data") .. "/mason/bin/gopls"
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "LSP rename"})
 
 vim.lsp.config("gopls", {
   cmd = { mason_bin },
