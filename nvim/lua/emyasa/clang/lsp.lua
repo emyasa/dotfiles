28349ae2810b01f74bad1e3240cd0233b90a956f
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-08-14 18:38:00 +0800

    c, c++ lsp

--- nvim/lua/emyasa/clang/lsp.lua
+++ nvim/lua/emyasa/clang/lsp.lua
@@ -1,4 +1,4 @@
-local clangd_bin = "/usr/bin/clangd"
+local clangd_bin = vim.fn.stdpath("data") .. "/mason/bin/clangd"
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
@@ -6,7 +6,7 @@ vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "LSP rename" })
 vim.keymap.set("n", "ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 vim.lsp.config("clangd", {
-  cmd = { clangd_bin, "--background-index", "--clang-tidy" },
+  cmd = { clangd_bin, "--background-index" },
   filetypes = { "c", "cpp", "objc", "objcpp" },
   root_dir = vim.fs.root(0, { "compile_commands.json", "compile_flags.txt", ".clangd", ".git" }),
 })
