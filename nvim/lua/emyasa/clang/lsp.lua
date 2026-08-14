56c7ad517a5a60d28cf06428d8ec397896a9527e
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2026-08-15 00:09:27 +0800

    misc

--- nvim/lua/emyasa/clang/lsp.lua
+++ nvim/lua/emyasa/clang/lsp.lua
@@ -6,7 +6,10 @@ vim.keymap.set("n", "rn", vim.lsp.buf.rename, { desc = "LSP rename" })
 vim.keymap.set("n", "ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 
 vim.lsp.config("clangd", {
-  cmd = { clangd_bin, "--background-index" },
+  cmd = {
+      clangd_bin,
+      "--background-index",
+  },
   filetypes = { "c", "cpp", "objc", "objcpp" },
   root_dir = vim.fs.root(0, { "compile_commands.json", "compile_flags.txt", ".clangd", ".git" }),
 })
